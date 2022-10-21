cfa6627519e0700578ce2fc84800e219ab6a5004
SZlonghua <1053595189@qq.com>
2022-10-21 14:46:48 +0800

    add class RequestBody,MediaType,HttpUrl

--- src/test/java/com/tiny/okhttp/OkHttpClientTest.java
+++ src/test/java/com/tiny/okhttp/OkHttpClientTest.java
@@ -18,8 +18,29 @@ public class OkHttpClientTest extends TestCase {
         }
     }
 
+
+    public static final MediaType JSON
+            = MediaType.get("application/json; charset=utf-8");
+
+    public String post(String url, String json) throws IOException {
+        OkHttpClient client = new OkHttpClient();
+
+        RequestBody body = RequestBody.create(JSON, json);
+        Request request = new Request.Builder()
+                .url(url)
+                .post(body)
+                .build();
+        try (Response response = client.newCall(request).execute()) {
+            return response.body().string();
+        }
+    }
+
     public void testGet() throws IOException {
         String result = get("http://localhost:8080/api/hello");
+        System.out.println(result);
+    }
+    public void testPost() throws IOException {
+        String result = post("http://localhost:8080/api/hello","{\"key\":\"liao\"}");
         System.out.println(result);
     }
 
